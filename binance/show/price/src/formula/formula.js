cc1567a5b74a546deddf15af37c8f74c2db67ef1
Henrique Santos De Faria <henriquesantosdefaria@MacBook-Pro-de-Henrique.local>
2021-06-30 03:31:04 -0300

    first commit

---
+++ binance/show/price/src/formula/formula.js
@@ -0,0 +1,17 @@
+const clc = require("cli-color")
+const fetch = require('node-fetch')
+
+function Run(inputCoin, inputCurrency) {
+    const coin = inputCoin.toUpperCase()
+    const currency = inputCurrency.toUpperCase()
+
+    fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${coin}${currency}`, {
+            method: 'GET'
+        })
+            .then(res => res.json())
+            .then(coin => console.log(new Intl.NumberFormat().format(coin.price)))
+            .catch(err => console.error("Sorry! Could not get the BTC price, please try again later."))
+}
+
+const formula = Run
+module.exports = formula
---
+++ binance/show/price/src/index.js
@@ -0,0 +1,6 @@
+const run = require("./formula/formula")
+
+const INPUT1 = process.env.RIT_INPUT_COIN
+const INPUT2 = process.env.RIT_INPUT_CURRENCY
+
+run(INPUT1, INPUT2)
